98706651cdf9d6d6eac376c45d418c44c0cce9e9
Zinevich Yan <facecrack1337@gmail.com>
2021-02-10 20:23:47 +0500

    + Add blur and pictures

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,10 +1,44 @@
 import Carousel from 'react-material-ui-carousel';
-import { Paper, Button } from '@material-ui/core'
+import styled from 'styled-components';
+import { Paper } from '@material-ui/core'
 import { FC } from 'react';
+
+const StyledItem = styled.div`
+  position: relative;
+  > h2 {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 1;
+    margin-left: 20px;
+  }
+
+  > p {
+    position: absolute;
+    top: 0;
+    left: 0;
+    margin-top: 50px;
+    z-index: 1;
+    margin-left: 20px;
+  }
+
+  > img {
+    width: 100%;
+    height: 400px;
+    filter: blur(5px);
+    z-index: -1;
+  }
+`; 
+
+type ItemType = { 
+  name: string, 
+  description: string, 
+  image: string,
+};
 
 type SliderProps = {
   children?: never;
-  items: { name: string, description: string }[]
+  items: ItemType[]
 }
 
 export const Slider: FC<SliderProps> = ({
@@ -12,26 +46,24 @@ export const Slider: FC<SliderProps> = ({
 }: SliderProps) => (
   <Carousel>
     {
-      items.map( (item, i) => <Item key={i} name={item.name} description={item.description} /> )
+      items.map( (item, i) => <Item key={i} name={item.name} description={item.description} image={item.image} /> )
     }
   </Carousel>
 )
 
 type ItemProps = {
   children?: never;
-  name: string;
-  description: string;
-}
+} & ItemType;
 
 const Item: FC<ItemProps> = ({
-  name, description
+  name, description, image,
 }: ItemProps) =>(
   <Paper>
-    <h2>{name}</h2>
-    <p>{description}</p>
+    <StyledItem>
+      <h2>{name}</h2>
+      <p>{description}</p>
 
-    <Button className="CheckButton">
-      Check it out!
-    </Button>
+      <img src={image} alt="carousel" />
+    </StyledItem>
   </Paper>
 )
--- src/pageParts/sales/Sales.tsx
+++ src/pageParts/sales/Sales.tsx
@@ -4,12 +4,12 @@ import { Slider } from "../../components";
 export const Sales: FC = () => (
   <Slider 
     items={[
-      {name: '123', description: '321'},
-      {name: '123', description: '321'},
-      {name: '123', description: '321'},
-      {name: '123', description: '321'},
-      {name: '123', description: '321'},
-      {name: '123', description: '321'},
+      {name: 'Скидка 10%', description: 'Описание для акции со скидкой в 10%', image: 'http://achtung.kz/wp-content/uploads/2016/01/%D0%90%D0%BA%D1%86%D0%B8%D1%8F.jpg'},
+      {name: 'Скидка 20%', description: 'Описание для акции со скидкой в 20%', image: 'http://connect-s.kz/uploads/posts/2017-09/1506000859_400x175xak_.jpg'},
+      {name: 'Скидка 30%', description: 'Описание для акции со скидкой в 30%', image: 'https://lh3.googleusercontent.com/proxy/MDoPkkg-0QU0JQHgxAuUG0HV4k8BAWwxpdfdE-2o63Wjr-R57Yl_ap52QGFjPfan12uaeMAuo0fJoerkm7SmLFSYxTGTlJ-soAn2YsaYvD7c_8ARGSUKQkmV'},
+      {name: 'Скидка 40%', description: 'Описание для акции со скидкой в 40%', image: 'https://lh3.googleusercontent.com/proxy/D3CZpt2qYuK30sSJ0ZjCsXoovcwftMCg-TGty5N_AZQEZV16M7NtxrTW5UoUezzRXtbqY0-sIIj_vCRosYXpnyLIna9xNAXhsXm0n1YIhKfjQV56sq0w8LaWrLc5udSkxg3SfOq4RXl0L2SskC_C'},
+      {name: 'Скидка 50%', description: 'Описание для акции со скидкой в 50%', image: 'https://lh3.googleusercontent.com/proxy/G9CYttorA2ycYShATE1C_e9acMKS-Y-k2TPbcUNq94ej5KeshHAXXROoQKgbcr_SdNZZEJ916pgsJYWCxCI'},
+      {name: 'Скидка 60%', description: 'Описание для акции со скидкой в 60%', image: 'https://bokaz.kz/wp-content/uploads/2017/04/sp-1.png'},
     ]}
   />
 )
